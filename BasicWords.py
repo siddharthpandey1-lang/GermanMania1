e7a8c23a045cac834ded68ee879ffc5c0192959f
Siddhart <Siddharth.pandey1@gmail.com>
2025-10-02 19:53:42 +0530

    gameBase

---
+++ BasicWords.py
@@ -0,0 +1,14 @@
+print("Now let's learn some basic words in German!")
+print("Here are some common words and their translations:")
+basic_words = {
+    "Hello": "Hallo",
+    "Goodbye": "Auf Wiedersehen",
+    "Please": "Bitte",
+    "Thank you": "Danke",
+    "Yes": "Ja",
+    "No": "Nein",
+    "Excuse me": "Entschuldigung",
+    "Sorry": "Es tut mir leid",
+    "Good morning": "Guten Morgen",
+    "Good night": "Gute Nacht"
+}
